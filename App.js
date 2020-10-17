902463f6995f4505b1c261ec6d6b5ab7c78b6305
NYardich <nickyardich@gmail.com>
2020-10-16 23:38:21 -0400

    basic stylesheet setup

--- App.js
+++ App.js
@@ -5,17 +5,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>ecohunt</Text>
+      <Text style={styles.subtitleText}>minimize overfishing and overhunting</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // {"Turquoise Green":"aad2ba","Black Chocolate":"0f1108","Fern Green":"3a7d44","Princeton Orange":"ed7d3a","Bistre":"311e10"}
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#aad2ba',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    fontSize: 80,
+    color: '#0f1108'
+  },
+  subtitleText: {
+    fontSize: 20,
+    color: '#3a7d44'
+  }
 });
